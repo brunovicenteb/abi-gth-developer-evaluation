a7d36ece244c92c728f71fdfd976bb7eb068b155
Bruno Belchior <brunovicenteb@gmail.com>
2025-06-04 18:20:14 -0300

    🗃️ feat(domain): Add sales domain layer

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Sales/Entities/Sale.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Sales.Entities;
+
+public class Sale : BaseEntity
+{
+    public const string MAX_ITEM_LIMIT_EXCEEDED = "Não é permitido vender mais de 20 unidades do mesmo item.";
+    public const string INVALID_DISCOUNT_RULE = "Desconto aplicado inválido para a quantidade informada.";
+    public const string EMPTY_SALE_ITEMS = "Uma venda deve conter ao menos um item.";
+
+    public string SaleNumber { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string Branch { get; set; } = string.Empty;
+    public List<SaleItem> Items { get; set; } = [];
+    public decimal TotalAmount { get; private set; }
+    public bool IsCancelled { get; private set; }
+
+    public void CalculateTotal()
+    {
+        if (!Items.Any())
+            throw new InvalidOperationException(EMPTY_SALE_ITEMS);
+
+        foreach (var item in Items)
+            item.CalculateTotal();
+
+        TotalAmount = Items.Sum(i => i.Total);
+    }
+
+    public void Cancel()
+    {
+        IsCancelled = true;
+    }
+}
