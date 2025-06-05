d6a4dbad37f332ac069c0aa692b1e45b29c00fd3
Bruno Belchior <brunovicenteb@gmail.com>
2025-06-05 18:35:36 -0300

    🗃️ feat: Get sale Application layers

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+public class GetSaleResult
+{
+    public Guid Id { get; set; }
+    public string SaleNumber { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string Branch { get; set; } = string.Empty;
+    public decimal TotalAmount { get; set; }
+    public List<GetSaleItemResult> Items { get; set; } = new();
+}
+
+public class GetSaleItemResult
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Discount { get; set; }
+    public decimal Total { get; set; }
+}
