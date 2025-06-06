1edb5b8d1bb5ca948a4c18212179673924786313
Bruno Belchior <brunovicenteb@gmail.com>
2025-06-06 09:46:57 -0300

    🗃️ feat: Search sales Application and Infra layers

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/BaseRepository.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/BaseRepository.cs
@@ -1,6 +1,8 @@
 using Ambev.DeveloperEvaluation.Domain.Common;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq.Dynamic.Core;
 
 namespace Ambev.DeveloperEvaluation.ORM.Repositories;
 
@@ -66,5 +68,60 @@ public abstract class BaseRepository<TContext, TEntity> : IBaseRepository<TEntit
     {
         int count = await Context.SaveChangesAsync(cancellationToken);
         return count > 0;
+    }
+
+    public virtual IQueryable<TEntity> GetQueryableCollection()
+    {
+        return Collection.AsQueryable();
+    }
+
+    public async Task<(IEnumerable<TEntity> Items, int Total)> GetPagedAsync(int page, int size, string orderBy, Dictionary<string, string> filters, CancellationToken cancellationToken)
+    {
+        var query = GetQueryableCollection();
+
+        query = ApplyFilters(filters, query);
+        query = ApplyOrderBy(orderBy, query);
+
+        var total = await query.CountAsync(cancellationToken);
+        var skip = (page - 1) * size;
+
+        var items = await query
+            .Skip(skip)
+            .Take(size)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        return (items, total);
+    }
+
+    private static IQueryable<TEntity> ApplyOrderBy(string orderBy, IQueryable<TEntity> query)
+    {
+        if (string.IsNullOrEmpty(orderBy))
+            return query;
+
+        var isDescending = orderBy.StartsWith("-");
+        var property = isDescending ? orderBy.Substring(1) : orderBy;
+        var direction = isDescending ? "descending" : "ascending";
+        query = query.OrderBy($"{property} {direction}");
+        return query;
+    }
+
+    private static IQueryable<TEntity> ApplyFilters(Dictionary<string, string> filters, IQueryable<TEntity> query)
+    {
+        if (filters is null || filters.Count == 0)
+            return query;
+
+        foreach (var filter in filters)
+        {
+            var value = filter.Value;
+            if (!value.StartsWith("*"))
+                query = query.Where($"{filter.Key} == @0", value);
+            else
+            {
+                var trimmed = value.TrimStart('*');
+                query = query.Where($"{filter.Key}.ToLower().Contains(@0)", trimmed.ToLower());
+            }
+        }
+        return query;
     }
 }
