2635d6df519e6db5d0ea3fdd9120a484c35b2b1e
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-07 17:33:08 -0300

    🚧 feat: Add create sales command.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+
+public class CreateSaleCommand : IRequest<CreateSaleResult>
+{
+    public string SaleNumber { get; set; } = string.Empty;
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string Branch { get; set; } = string.Empty;
+    public List<CreateSaleItemDto> Items { get; set; } = [];
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleHandler.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.customers.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Sales.Entities;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+
+/// <summary>
+/// Handler for create a sale.
+/// </summary>
+public class CreateSaleHandler : IRequestHandler<CreateSaleCommand, CreateSaleResult>
+{
+    private readonly ISaleRepository _saleRepository;
+    private readonly IMapper _mapper;
+
+    public CreateSaleHandler(ISaleRepository saleRepository, IMapper mapper)
+    {
+        _saleRepository = saleRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<CreateSaleResult> Handle(CreateSaleCommand command, CancellationToken cancellationToken)
+    {
+        var validator = new CreateSaleValidator();
+        var validationResult = await validator.ValidateAsync(command, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var sale = _mapper.Map<Sale>(command);
+
+        sale.CalculateTotal();
+
+        await _saleRepository.AddAsync(sale, cancellationToken);
+
+        return _mapper.Map<CreateSaleResult>(sale);
+    }
+}
