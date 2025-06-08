8524891d4fb736632870f830337d5d00bd544a3b
Bruno Belchior <brunovicenteb@gmail.com>
2025-06-08 11:59:48 -0300

    🚧 feat: Add get sales query. (#9)
    * 🚧 feat: Add get sales query.
    
    * ✅ test: Add unit test.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSales/GetSalesHandler.cs
@@ -0,0 +1,40 @@
+﻿using Ambev.DeveloperEvaluation.Domain.customers.Repositories;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSales
+{
+    public class GetSalesHandler : IRequestHandler<GetSalesQuery, GetSalesResult>
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMapper _mapper;
+
+        public GetSalesHandler(ISaleRepository saleRepository, IMapper mapper)
+        {
+            _saleRepository = saleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetSalesResult> Handle(GetSalesQuery query, CancellationToken cancellationToken)
+        {
+            var validator = new GetSalesQueryValidator();
+            var validationResult = await validator.ValidateAsync(query, cancellationToken);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            var (items, total) = await _saleRepository.GetPagedAsync(query.Page, query.Size,
+                query.OrderBy, query.Filters, cancellationToken);
+
+            var data = _mapper.Map<List<SaleListItemDto>>(items);
+            return new GetSalesResult
+            {
+                Page = query.Page,
+                Size = query.Size,
+                Total = total,
+                Data = data
+            };
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSales/GetSalesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSales
+{
+    public class GetSalesQuery : IRequest<GetSalesResult>
+    {
+        public Dictionary<string, string>? Filters { get; set; }
+        public string? OrderBy { get; set; }
+        public int Page { get; set; } = 1;
+        public int Size { get; set; } = 10;
+    }
+}
