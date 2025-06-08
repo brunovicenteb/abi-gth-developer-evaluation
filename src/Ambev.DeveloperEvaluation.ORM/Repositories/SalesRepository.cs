a44f9420ee4269c2de29eac1cd5f86590db67f45
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-08 12:11:22 -0300

    🚧 feat: Add create sale feature on driver layer.

---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/SalesRepository.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.customers.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Sales.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+public class SalesRepository : BaseRepository<DefaultContext, Sale>, ISaleRepository
+{
+    private const string DEFAULT_ORDER_BY = "-CreatedAt";
+
+    public SalesRepository(DefaultContext context) : base(context)
+    {
+    }
+
+    protected override string GetDefaultOrderBySearch() => DEFAULT_ORDER_BY;
+
+    protected override DbSet<Sale> Collection => Context.Sales;
+
+    public override IQueryable<Sale> GetQueryableCollection()
+    {
+        return Collection
+            .Where(sale => !sale.IsCancelled)
+            .AsNoTracking();
+    }
+
+    public override async Task<Sale> GetByIdAsync(Guid id, bool trackedItem, CancellationToken cancellationToken)
+    {
+        if (trackedItem)
+            return await Collection
+                .Where(sale => !sale.IsCancelled && sale.Id == id)
+                .Include(sale => sale.Items.Where(item => !item.IsCancelled))
+                .FirstOrDefaultAsync(cancellationToken);
+        return await Collection
+            .Where(sale => !sale.IsCancelled && sale.Id == id)
+            .Include(sale => sale.Items.Where(item => !item.IsCancelled))
+            .AsNoTracking()
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/SalesController.cs
@@ -0,0 +1,59 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales;
+
+/// <summary>
+/// Controller responsible for managing operations related to sales,
+/// including creation, updates, cancellations, and retrieval.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class SalesController : BaseController
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SalesController"/>.
+    /// </summary>
+    /// <param name="mediator">The MediatR handler for command/query dispatching.</param>
+    /// <param name="mapper">The AutoMapper instance for DTO conversion.</param>
+    public SalesController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Creates a new sale.
+    /// </summary>
+    /// <param name="request">Request payload containing sale data.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Details of the created sale.</returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(ApiResponseWithData<CreateSaleResponse>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateSale([FromBody] CreateSaleRequest request, CancellationToken cancellationToken)
+    {
+        var validator = new CreateSaleRequestValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult.Errors);
+
+        var command = _mapper.Map<CreateSaleCommand>(request);
+        var result = await _mediator.Send(command, cancellationToken);
+
+        return Created(string.Empty, new ApiResponseWithData<CreateSaleResponse>
+        {
+            Success = true,
+            Message = "Venda criada com sucesso",
+            Data = _mapper.Map<CreateSaleResponse>(result)
+        });
+    }
+}
