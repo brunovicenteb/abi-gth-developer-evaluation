6ceed6d65211cb40f67e816f5b9266c45d28c44e
Bruno Belchior <brunovicenteb@gmail.com>
2025-06-08 11:56:41 -0300

    🚧 feat: Add get sale query. (#8)
    * 🚧 feat: Add get sale query.
    
    * ✅ test: Add unit test.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSale/GetSaleProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Sales.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSale;
+
+public class GetSaleProfile : Profile
+{
+    public GetSaleProfile()
+    {
+        CreateMap<Sale, GetSaleResult>();
+        CreateMap<SaleItem, GetSaleItemResult>();
+    }
+}
