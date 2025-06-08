5f98a5dbd9581d74e59b95cef7135b3e372d6f44
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-08 19:24:31 -0300

    🐛 fix: Fix some bugs.

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/UpdateSale/UpdateSaleItemRequest.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/UpdateSale/UpdateSaleItemRequest.cs
@@ -2,7 +2,7 @@
 
 public class UpdateSaleItemRequest
 {
-    public int ProductId { get; set; }
+    public Guid ProductId { get; set; }
     public string ProductName { get; set; } = string.Empty;
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
