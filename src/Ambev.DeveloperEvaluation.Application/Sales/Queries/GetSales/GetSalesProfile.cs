923c6837c24489b3548f43b430edd4801374839f
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-08 11:57:58 -0300

    🚧 feat: Add get sales query.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSales/GetSalesProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Sales.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSales
+{
+    public class GetSalesProfile : Profile
+    {
+        public GetSalesProfile()
+        {
+            CreateMap<Sale, SaleListItemDto>();
+        }
+    }
+}
