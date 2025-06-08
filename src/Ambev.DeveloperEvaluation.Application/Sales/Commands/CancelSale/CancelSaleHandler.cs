d11b380c3bf47e8816cd855dd665069d9e4d7825
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-08 11:44:11 -0300

    🚧 feat: Add cancel sales command.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Commands/CancelSale/CancelSaleHandler.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.DeveloperEvaluation.Domain.customers.Repositories;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Commands.CancelSale;
+
+/// <summary>
+/// Handler for cancel a sale.
+/// </summary>
+public class CancelSaleHandler : IRequestHandler<CancelSaleCommand, CancelSaleResponse>
+{
+    private readonly ISaleRepository _saleRepository;
+
+    public CancelSaleHandler(ISaleRepository saleRepository)
+    {
+        _saleRepository = saleRepository;
+    }
+
+    public async Task<CancelSaleResponse> Handle(CancelSaleCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new CancelSaleValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var sale = await _saleRepository.GetByIdAsync(request.Id, false, cancellationToken)
+            ?? throw new KeyNotFoundException($"Venda com ID {request.Id} não encontrada.");
+
+        sale.Cancel();
+
+        await _saleRepository.UpdateAsync(sale, cancellationToken);
+
+        return new CancelSaleResponse(true);
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Commands/CancelSale/CancelSaleValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Commands.CancelSale;
+
+/// <summary>
+/// Validator for DeleteSaleCommand.
+/// </summary>
+public class CancelSaleValidator : AbstractValidator<CancelSaleCommand>
+{
+    public CancelSaleValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("O ID da venda é obrigatório.");
+    }
+}
