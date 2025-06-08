288748a1b320199e33613c0f9ea8d293b7cc5208
Bruno Belchior de Souza Vicente <brunovicenteb@gmail.com>
2025-06-08 11:31:24 -0300

    🚧 feat: Add update sales command.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Commands/UpdateSale/UpdateSaleCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Commands.UpdateSale;
+
+public class UpdateSaleCommand : IRequest<UpdateSaleResult>
+{
+    public Guid Id { get; set; }
+    public string SaleNumber { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string Branch { get; set; } = string.Empty;
+    public List<UpdateSaleItemDto> Items { get; set; } = new();
+}
